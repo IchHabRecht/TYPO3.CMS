1e03167500f4b496010b146638b6360a7efa0d20
Anja Leichsenring <aleichsenring@ab-softlab.de>
2016-02-25 22:11:52 +0100

    Revert "[SECURITY] XML entity expansion"
    This reverts commit 64fc8373918cb239f5c7cd559257c56316b7c419.
    The patch raises the minimum php version requirement to PHP 5.2.11.
    This is not given for everyone, so the patch gets reverted for a special
    branch maintaining compatibility with PHP 5.2.0

--- typo3/sysext/adodb/adodb/adodb-xmlschema.inc.php
+++ typo3/sysext/adodb/adodb/adodb-xmlschema.inc.php
@@ -1456,8 +1456,6 @@ class adoSchema {
 		$this->success = 2;
 
 		$xmlParser = $this->create_parser();
-		 // Disables the functionality to allow external entities to be loaded when parsing the XML, must be kept
-		$previousValueOfEntityLoader = libxml_disable_entity_loader(TRUE);
 
 		// Process the file
 		while( $data = fread( $fp, 4096 ) ) {
@@ -1470,7 +1468,6 @@ class adoSchema {
 			}
 		}
 
-		libxml_disable_entity_loader($previousValueOfEntityLoader);
 		xml_parser_free( $xmlParser );
 
 		return $this->sqlArray;
@@ -1505,8 +1502,6 @@ class adoSchema {
 		$this->success = 2;
 
 		$xmlParser = $this->create_parser();
-		// Disables the functionality to allow external entities to be loaded when parsing the XML, must be kept
-		$previousValueOfEntityLoader = libxml_disable_entity_loader(TRUE);
 
 		if( !xml_parse( $xmlParser, $xmlstring, TRUE ) ) {
 			die( sprintf(
@@ -1516,7 +1511,6 @@ class adoSchema {
 			) );
 		}
 
-		libxml_disable_entity_loader($previousValueOfEntityLoader);
 		xml_parser_free( $xmlParser );
 
 		return $this->sqlArray;
