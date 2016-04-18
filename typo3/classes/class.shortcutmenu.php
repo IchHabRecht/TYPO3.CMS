IchHabRecht/TYPO3.CMS

commit f7d86a3b8ac6c7cfdc9f01be844e8c98112cbc2d

[SECURITY] Stored XSS in shortcut functionality
Resolves: #73449
Releases: 4.5
Change-Id: Ibb2211be5aca31b70b76a73ac745898c994f4b3b

typo3/classes/class.shortcutmenu.php
@@ -332,7 +332,7 @@ class ShortcutMenu implements backend_toolbarItem {
 			$shortcut['group']     = $shortcutGroup;
 			$shortcut['icon']      = $this->getShortcutIcon($row, $shortcut);
 			$shortcut['iconTitle'] = $this->getShortcutIconTitle($shortcutLabel, $row['module_name'], $row['M_module_name']);
-			$shortcut['action']    = 'jump(unescape(\''.rawurlencode($row['url']).'\'),\''.implode('_',$moduleParts).'\',\''.$moduleParts[0].'\');';
+			$shortcut['action']    = "jump('" . t3lib_div::quoteJSvalue($row['url']) . "','" . t3lib_div::quoteJSvalue(implode('_', $moduleParts)) . "','" . t3lib_div::quoteJSvalue($moduleParts[0]) . "');";
 
 			$lastGroup   = $row['sc_group'];
 			$shortcuts[] = $shortcut;

commit d9118fb9afa4fcdc5c7135a41bb78c201d3390e6

[BUGFIX] Fix regression in shortcut menu
Fixes a regression which was introduced by
f7d86a3b8ac6c7cfdc9f01be844e8c98112cbc2d of the 4.5.45 release
when a XSS vulnerability was tackled in the shortcur menu.

Resolves: #T11

typo3/classes/class.shortcutmenu.php
@@ -332,7 +332,7 @@ class ShortcutMenu implements backend_toolbarItem {
 			$shortcut['group']     = $shortcutGroup;
 			$shortcut['icon']      = $this->getShortcutIcon($row, $shortcut);
 			$shortcut['iconTitle'] = $this->getShortcutIconTitle($shortcutLabel, $row['module_name'], $row['M_module_name']);
-			$shortcut['action']    = "jump('" . t3lib_div::quoteJSvalue($row['url']) . "','" . t3lib_div::quoteJSvalue(implode('_', $moduleParts)) . "','" . t3lib_div::quoteJSvalue($moduleParts[0]) . "');";
+			$shortcut['action']    = "jump(" . t3lib_div::quoteJSvalue($row['url']) . "," . t3lib_div::quoteJSvalue(implode('_', $moduleParts)) . "," . t3lib_div::quoteJSvalue($moduleParts[0]) . ")";
 
 			$lastGroup   = $row['sc_group'];
 			$shortcuts[] = $shortcut;