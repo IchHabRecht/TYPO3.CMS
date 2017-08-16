bf2c28bf97140c7dfaffd10e27b58a8e697934b0
Anja Leichsenring <aleichsenring@ab-softlab.de>
2017-08-16 17:38:49 +0200

    [BUGFIX] HtmlArea: Use getAttribute() directly
    RTE HtmlArea uses getAttributeNode() for gecko
    browsers, however this is deprecated in current
    Firefox versions.
    
    To just get the value of an attribute getAttribute()
    can be used instead:
    
    See the usages here:
    - https://www.w3schools.com/jsref/met_element_getattributenode.asp
    - https://www.w3schools.com/jsref/met_element_getattribute.asp
    
    Resolves: #80934
    Releases: 7.6
    Change-Id: Ic3962ad7339a56c8d0e7b94a8aaba27a56744bf8
    Reviewed-on: https://review.typo3.org/52814
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Riccardo De Contardi <erredeco@gmail.com>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Frank Naegler <frank.naegler@typo3.org>
    Tested-by: Frank Naegler <frank.naegler@typo3.org>

--- typo3/sysext/rtehtmlarea/htmlarea/plugins/TYPO3Link/typo3link.js
+++ typo3/sysext/rtehtmlarea/htmlarea/plugins/TYPO3Link/typo3link.js
@@ -326,7 +326,7 @@ HTMLArea.TYPO3Link = Ext.extend(HTMLArea.Plugin, {
 					}
 				}
 				if (Ext.isGecko) {
-					node.href = decodeURI(node.getAttributeNode('href').value);
+					node.href = decodeURI(node.getAttribut('href'));
 				}
 				if (cur_target.trim()) node.target = cur_target.trim();
 					else node.removeAttribute('target');
